42b836543fa163b1b982ec2164d8c94f66645814
Technoking <keyvelbusiness@gmail.com>
2021-11-20 14:48:00 -0600

    first app

--- src/Home.tsx
+++ src/Home.tsx
@@ -174,7 +174,10 @@ const Home = (props: HomeProps) => {
         <p>Wallet {shortenAddress(wallet.publicKey.toBase58() || "")}</p>
       )}
 
-     
+      {wallet && <p>Balance: {(balance || 0).toLocaleString()} SOL</p>}
+
+      {wallet && <p>Total Available: {itemsAvailable}</p>}
+
       {wallet && <p>Redeemed: {itemsRedeemed}</p>}
 
       {wallet && <p>Remaining: {itemsRemaining}</p>}
